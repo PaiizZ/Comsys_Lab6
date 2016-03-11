b7db00179e7b4326ad1d56ffff381736a7e051a7
paiizz <wanchanapon.t@ku.th>
2016-03-11 23:03:29 +0700

    add enable to alu and multiplier

--- add_sub/multiplier.h
+++ add_sub/multiplier.h
@@ -1,80 +1,94 @@
-#ifndef MULTIPLY_H
-#define MULTIPLY_H
+#ifndef MULTIPLIER_H
+#define MULTIPLIER_H
 
 #include <systemc.h>
 #include "alu.h"
 SC_MODULE(multiplier) {
+	// input
 	sc_in<sc_uint<16> > ain, bin;
-	sc_signal<sc_uint<16> > a,q,m,aa,mm,ss;
-  sc_signal<sc_uint<4> > control;
-	sc_uint<16> _q, _m,_a;
-
+	/*
+		signal to alu
+		this:alu
+		aa = ain
+		mm = bin
+		ss = sum
+	*/
+	sc_signal<sc_uint<16> > aa, mm, ss;
+	// control the alu
+	sc_signal<sc_uint<4> > control;
+	// local var to store the value
+	sc_uint<16> _q, _m, _a;
+	// carry in/out
 	bool c;
+	// output to the caller
 	sc_out<sc_uint<32> > out;
+	//alu
 	alu ALUA;
-	sc_signal<bool> aci, aco, oflag, lflag, zflag;
+	//flag
+	sc_signal<bool> oflag, lflag, zflag , enable;
 	void p1() {
-
+		//init variable
 		_q = ain.read();
 		_m = bin.read();
 		_a = 0b0000000000000000;
 		c = 0b00;
-		//cout << "_M: " << _m << " " << "_Q: " << _q << " _A: " << _a << endl;
-
-		/*q.write(ain.read());
-		m.write(bin.read());*/
-		/*q.write(_q);
-		m.write(_m);*/
-		/*
-		while (true) {
-			sc_start(100, SC_NS);
-		}*/
-		//cout << "M: " << m.read()<< " " << "Q: " << q.read() << " A: "<<ain.read()<< endl;
+		// run the algo for number of bit
 		for (int i = 0; i < 16; i++) {
+			//if q0 == 1
+			//a = a+m
 			if (_q[0]) {
 				aa = _m;
 				mm = _a;
+				enable.write(!enable);
+				sc_start(100, SC_NS);
 				_a = ss;
 				c = oflag;
 			}
-			/*sc_uint<16> sq = q.read();*/
-			//sq = sq >> 1;
+			/*
+				shift
+				C  A    Q    M
+				1 1011 1001 1011 init
+				0 1101 1100 1011 shft
+			*/
+			//shift q
 			_q = _q >> 1;
+			// shift last bit of a to first bit of q
 			_q[15] = _a[0];
-			/*sc_uint<16> sa = a.read();
-			sa = sa >> 1;
-			sq[15] = sa[0];
-			sa[15] = c;
-			c = false;
-			q.write(sq);
-			a.write(sa);*/
+			//shift a
 			_a = _a >> 1;
+			//shift carry to first bit of a
 			_a[15] = c;
 			c = false;
 		}
+		//combine q and a to the out put
+		//						a								q
+		//			 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0][0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		// out[32] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
 		sc_uint<32> outi;
 		for (int i = 0; i < 15; i++) {
 			outi[i] = _q[i];
 		}
 		for (int i = 0; i < 15; i++) {
-			outi[16+i] = _a[i];
+			outi[16 + i] = _a[i];
 		}
 		out.write(outi);
 	}
-	SC_CTOR(multiplier): ALUA("ALU") {
-		SC_METHOD(p1);
+	SC_CTOR(multiplier) : ALUA("ALU") {
+
+		// bind ALU port
 		ALUA.ain(aa);
 		ALUA.bin(mm);
-		//ALU.ci(aci)
 		ALUA.sum(ss);
+		ALUA.enable(enable);
 		control.write(0);
 		ALUA.control(control);
-		//ALU.co(aco);
 		ALUA.zflag(zflag);
 		ALUA.oflag(oflag);
 		ALUA.lflag(lflag);
 
-		sensitive << ain << bin << a << q << m << aa << mm << ss << out << aci << aco ;
+		SC_METHOD(p1);
+		//add sensitive
+		sensitive << ain << bin ;
 	}
 };
 
--- add_sub/sc_main.cpp
+++ add_sub/sc_main.cpp
@@ -1,55 +1,41 @@
 #include <systemc.h>
 
-#include "adder.h"
 #include "multiplier.h"
 
 int sc_main(int argc, char* argv[])
 {
-	sc_signal<sc_uint<16> > ain, bin, sum;
+	sc_signal<sc_uint<16> > ain, bin ;
 	sc_signal<sc_uint<32> > out;
-	sc_signal<bool> ci, co;
-	sc_signal<bool> zflag, oflag, lflag;
-	sc_signal<bool> as;
 	sc_clock clk("clk", 10, SC_NS, 0.5);   // Create a clock signal
 
-
+	//trace
 	sc_trace_file *fp;
 	fp = sc_create_vcd_trace_file("add_sub_wave");
 	fp->set_time_unit(1, SC_PS);
 	sc_trace(fp, ain, "ain");
 	sc_trace(fp, bin, "bin");
-	sc_trace(fp, sum, "sum");
-	sc_trace(fp, ci, "ci");
-	sc_trace(fp, co, "co");
-	sc_trace(fp, zflag, "zflag");
-	sc_trace(fp, oflag, "oflag");
-	sc_trace(fp, lflag, "lflag");
 	sc_trace(fp, clk, "clk");
 
 
-
+	//bind multoplier
 	multiplier mul("multiplier");
 	mul.ain(ain);
 	mul.bin(bin);
 	mul.out(out);
 
-	ain.write(4);
-	bin.write(3);
-	sc_start(100, SC_NS);               // Run simulation
+	//test : multiplication table
+	for (int i = 1; i < 13; i++) {
+	for (int j = 2; j < 13; j++) {
+		/* code */
+		ain.write(j);
+		bin.write(i);
+		sc_start(1000, SC_NS);               // Run simulation
+		cout << ain << "*" << bin << " = " << out << " " ;
+	}
+	cout << endl;
+}
 
-	cout << ain << "*" << bin << "=" << out << endl;
 
-	ain.write(2);
-	bin.write(3);
-//	cout << ain << "*" << bin << "=" << out;
-	sc_start(100, SC_NS);               // Run simulation
 
-	cout << ain << "*" << bin << "=" << out<< endl;
-	ain.write(5);
-	bin.write(3);
-//	cout << ain << "*" << bin << "=" << out;
-	sc_start(100, SC_NS);               // Run simulation
-
-	cout << ain << "*" << bin << "=" << out<< endl;
 	return 0;                           // Return OK, no errors.
 }
